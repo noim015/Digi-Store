51340e320b6443c544209ea509cfbfc61fce22f5
imtiaz <sayed.noim.ahmed@gmail.com>
2021-09-13 12:10:57 +0600

    Average rating and total rating count done

--- js/app.js
+++ js/app.js
@@ -14,12 +14,14 @@ const showProducts = (products) => {
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image" src="${image}" alt="Product Image">
-      </div>
+      
+       <img class="product-image" src="${image}" alt="Product Image">
+      
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
+      <h4>Average Rating: ${product.rating['rate']}</h4>
+      <h5>Total Rating Count: ${product.rating['count']}</h5>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
